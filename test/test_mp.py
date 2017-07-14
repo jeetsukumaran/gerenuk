9f7f3936102cd19392b9a1c00920be8cdd1887f6
Jeet Sukumaran <jeetsukumaran@gmail.com>
2017-07-14 00:50:52 -0400

    Test framework

--- test/test_mp.py
+++ test/test_mp.py
@@ -1,22 +1,28 @@
 
 import unittest
+import time
+from collections import Counter
 from gerenuk import simulate
 
 class TestWorker(simulate.SimulationWorker):
 
     def simulate(self):
-        return [self.name, self.num_tasks_received]
+        time.sleep(0.0001)
+        return {
+                "name": self.name,
+                "task_count": self.num_tasks_received,
+                }
 
 class MpArchitectureTests(unittest.TestCase):
 
-    def test_run(self):
+    def test_workers_used(self):
         config_d = {
                 "name": "test",
                 "standard_error_logging_level": "warning",
                 "log_to_file": False,
                 }
-        nreps = 1000
-        num_processes = 10
+        nreps = 10000
+        num_processes = 2
         gs = simulate.GerenukSimulator(
                 config_d=config_d,
                 num_processes=num_processes,
@@ -24,6 +30,10 @@ class MpArchitectureTests(unittest.TestCase):
         gs.worker_class = TestWorker
         results = gs.execute(nreps)
         self.assertEqual(len(results), nreps)
+        counter = Counter()
+        for result in results:
+            counter[result["name"]] += 1
+        self.assertEqual(len(counter), num_processes)
 
 if __name__ == "__main__":
     unittest.main()
