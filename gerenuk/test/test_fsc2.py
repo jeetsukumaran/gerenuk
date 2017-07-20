6ed4dfc2788dee7d89a13f2205778170f1236c5e
Jeet Sukumaran <jeetsukumaran@gmail.com>
2017-07-20 02:56:12 -0400

    Rename files

--- gerenuk/test/test_fsc2.py
+++ gerenuk/test/test_fsc2.py
@@ -11,31 +11,31 @@ class Fsc2SiteFilepathTestCase(unittest.TestCase):
 
     def setUp(self):
         self.fsc = simulate.Fsc2Handler(
-                name="test-data-1",
+                name="test-one",
                 fsc2_path="fsc25",
                 working_directory=TESTS_DATA_DIR,
                 )
     def test_parameter_filepath(self):
-        self.assertEqual(self.fsc.parameter_filepath, "test-data-1.par")
+        self.assertEqual(self.fsc.parameter_filepath, "test-one.par")
 
     def test_results_dirpath(self):
-        self.assertEqual(self.fsc.results_dirpath, "test-data-1")
+        self.assertEqual(self.fsc.results_dirpath, "test-one")
 
     def test_deme0_derived_allele_frequency_filepath(self):
         self.assertEqual(self.fsc.deme0_derived_allele_frequency_filepath,
-                os.path.join("test-data-1", "test-data-1_DAFpop0.obs"))
+                os.path.join("test-one", "test-one_DAFpop0.obs"))
 
     def test_deme0_derived_allele_frequency_filepath(self):
         self.assertEqual(self.fsc.deme0_derived_allele_frequency_filepath,
-                os.path.join("test-data-1", "test-data-1_DAFpop0.obs"))
+                os.path.join("test-one", "test-one_DAFpop0.obs"))
 
     def test_deme1_derived_allele_frequency_filepath(self):
         self.assertEqual(self.fsc.deme1_derived_allele_frequency_filepath,
-                os.path.join("test-data-1", "test-data-1_DAFpop1.obs"))
+                os.path.join("test-one", "test-one_DAFpop1.obs"))
 
     def test_joint_derived_allele_frequency_filepath(self):
         self.assertEqual(self.fsc.joint_derived_allele_frequency_filepath,
-                os.path.join("test-data-1", "test-data-1_jointDAFpop1_0.obs"))
+                os.path.join("test-one", "test-one_jointDAFpop1_0.obs"))
 
 if __name__ == "__main__":
     unittest.main()
